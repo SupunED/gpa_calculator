d376f47eb873419ee5884ee9c94f9ce402bed16d
Supun Jayawardana <supunpramudithajayawardhana@gmail.com>
2025-10-26 15:58:54 +0530

    Implement login, register, logout backend functionality and index page personal greeting

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, redirect, session
+from flask_session import Session
 import sqlite3
-from config import login_required
+from config import login_required, get_db
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
-# Get sqlite3 database Conection
-con = sqlite3.connect("gpa.db")
-# create a cursor
-cursor = con.cursor()
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 @login_required
 def index():
-    return render_template("index.html")
+    user_id = session.get('user_id')
+
+    con = get_db()
+    cursor = con.cursor()
+
+    user_data = cursor.execute('SELECT firstname, lastname FROM student WHERE id = ?', (user_id,)).fetchone()
+    greet = f"Welcome, {user_data['firstname']} {user_data['lastname']}" if user_data else ""
+    con.close()
+
+    return render_template("index.html", greeting=greet)
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -23,8 +35,27 @@ def login():
     
     # If user submits login credentials via form POST
     else:
-        # Yet to implement
-        pass
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        
+        con = get_db()
+        cursor = con.cursor()
+
+        result = cursor.execute('SELECT * FROM student WHERE email = ?', (email,)).fetchone()
+        if not result or not check_password_hash(result["password_hashed"], password):
+            flash("Invalid Username/Password Combination!")
+            return redirect("/login")
+        
+        session['user_id'] = result["id"]
+        return redirect("/")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
     
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -35,19 +66,54 @@ def register():
     
     # if the user submits registration form and submits through form POST
     else:
-        # Yet to implement
-        pass
+        # take the user input, validate, register and redirect to home
+
+        first_name = request.form.get("fname")
+        last_name = request.form.get("lname")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        password_confirmation = request.form.get("confirmation")
+
+        # form validation
+        if not (first_name and last_name and email and password and password_confirmation):
+            flash("All fields are required!")
+            return redirect("/register")
+        if password != password_confirmation:
+            flash("Passwords do not match!")
+            return redirect("/register")
+
+        password_hashed = generate_password_hash(password=password, method="pbkdf2", salt_length=16)
+
+        con = get_db()
+        cursor = con.cursor()
+
+        exist_email = cursor.execute('SELECT * FROM student WHERE email = ?', (email,)).fetchone()
+
+
+        if exist_email:
+            flash("Email Already Exists!")
+            con.close()
+            return redirect("/register")
+        
+        # Insert new user
+        cursor.execute('INSERT INTO student (email, password_hashed, firstname, lastname) VALUES (?, ?, ?, ?)', (email, password_hashed, first_name, last_name))
+        con.commit()
+
+        # Get user id
+        user_data = cursor.execute('SELECT id FROM student WHERE email = ?', (email,)).fetchone()
+        session["user_id"] = user_data["id"]
+
+        con.close()
+        return redirect("/")
     
 
 @app.route("/ongoing")
 @login_required
 def ongoing():
-    # Yet to implement
-    pass
+    return render_template("ongoing.html")
 
 
 @app.route("/results")
 @login_required
 def results():
-    # Yet to implement
-    pass
+    return render_template("results.html")
--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 from flask import render_template, session, redirect
 from functools import wraps
+import sqlite3
+
 
 def login_required(f):
     # decorate routes to require login
@@ -9,3 +11,8 @@ def login_required(f):
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
+
+def get_db():
+    conn = sqlite3.connect("gpa.db", check_same_thread=False)
+    conn.row_factory = sqlite3.Row
+    return conn
